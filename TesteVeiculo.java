09ee8c1d130b03b49c1433bc2f985ff9738362e1
Júlia Rosa <julia.britorosa@gmail.com>
2023-07-10 13:01:18 -0300

    Update TesteVeiculo.java

--- TesteVeiculo.java
+++ TesteVeiculo.java
@@ -3,28 +3,71 @@ import java.util.Scanner;
 
 public class TesteVeiculo {
 
-	public static void main(String[] args) {
-		
-		Veiculos[] lista = new Veiculos[50];
+public static void main(String args[]){
+               
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+        int numVeiculos;
 
-		int tipo;
+        System.out.println("Digite o número de veículos a serem cadastrados:");
+        numVeiculos = scanner.nextInt();
+       
+        scanner.nextLine(); 
+        Veiculos[] veiculos = new Veiculos[numVeiculos];
+       
 
-		for (int i=0; i < 2; i++){
 
-			System.out.printf("Digite o numero 1 para Moto e o 2 para Carro");
-			Scanner in = new Scanner(System.in);
-			tipo = in.nextInt();
+        for (int i = 0; i < numVeiculos; i++) {
+            System.out.println("Digite 1 para moto ou 2 para carro:");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); 
+            
+            if (opcao == 1) {
+                Moto moto = new Moto();
+                moto.inserirDados();
+                veiculos[i] = moto;
+                
+            } else if (opcao == 2) {
+                Carro carro = new Carro();
+                carro.inserirDados();
+                veiculos[i] = carro;
+            }
+        }
 
-			if (1 == tipo) 
-				lista[i] = new Moto();
+        double totalPreco = 0.0;
 
-			else 
-				lista[i] = new Carro();
-			lista[i].inserirDados(); 
-		}
+        for (Veiculos veiculo : veiculos) {
+            veiculo.printDados();
+            totalPreco += veiculo.getPreco();
+        }
 
-		for (int i=0; i < 2; i++)
-			lista[i].printDados();
-	}
+        System.out.println("Total de preços antes dos ajustes: " + totalPreco);
 
-} 
+        for (Veiculos veiculo : veiculos) {
+      
+            if (veiculo instanceof Moto) {
+                Moto moto = (Moto) veiculo;
+                if (moto.getAno() >= 2008) {
+                    moto.reajustarPreco();
+                }
+                moto.printDados();
+               
+            } else if (veiculo instanceof Carro) {
+                Carro carro = (Carro) veiculo;
+                if (carro.getKm() > 100000) {
+                    carro.reduzirPreco();
+                }
+                carro.printDados();
+            }
+        }
+
+        double totalPrecoAjustado = 0.0;
+
+        for (Veiculos veiculo : veiculos) {
+            veiculo.printDados();
+            totalPrecoAjustado += veiculo.getPreco();
+        }
+
+        System.out.println("Total de preços após os ajustes: " + totalPrecoAjustado);
+	}  
+}
