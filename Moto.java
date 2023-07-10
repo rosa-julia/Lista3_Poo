9e94957cfdc309ebc24f3191c2c0df9c2538c743
Júlia Rosa <julia.britorosa@gmail.com>
2023-07-10 13:03:39 -0300

    Update Moto.java

--- Moto.java
+++ Moto.java
@@ -3,33 +3,43 @@ import java.util.Scanner;
 
 public class Moto extends Veiculos{
 	private int ano;
-	
+
 	public Moto(String modelo, double preco, int ano){
 		super(preco, modelo);
 		this.ano = ano;
 	}
-	
+
 	public Moto(){ 
 		this("Vazio",0.00,0); 
 	}
-	
-	public double getAno(){ 
-		return ano; 
-	}
+
+
 	public double getPreco(){ 
+		double precoBase = super.getPreco();
 		if(ano >= 2008) {
-			return super.getPreco() + super.getPreco() * 0.1;
+			return precoBase + precoBase * 0.1;
 		} else {
-			return getPreco(); 
+			return precoBase; 
 		}
 	}
-	
+
 	public void setAno(int ano){
 		this.ano = ano;
 	}
-	
 
-	
-	
+	public void reajustarPreco() {
+		setPreco(getPreco() + getPreco() * 0.1); 
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void inserirDados(){
+		Scanner in = new Scanner(System.in);
+		System.out.printf("Digite o ano: ");
+		ano = in.nextInt();
+		super.inserirDados();
+	}
 
 }
