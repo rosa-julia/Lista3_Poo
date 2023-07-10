c09f7be7849e557f3b6aa5c54013b347582759da
Júlia Rosa <julia.britorosa@gmail.com>
2023-07-10 13:03:12 -0300

    Update Carro.java

--- Carro.java
+++ Carro.java
@@ -1,30 +1,35 @@
 package lista3;
 
 public class Carro extends Veiculos{
-	
+
 	private double km;
-	
+
 	public Carro(String modelo,double preco, double km){
 		super(preco, modelo);
 		this.km = km;
 	}
-	
+
 	public Carro(){ 
 		this("Vazio",0.00,0.00); 
 	}
 	public void setKm(double km) {
 		this.km = km;
 	}
-	
+
 	public double getPreco(){ 
+		double precoBase = super.getPreco();
 		if(km > 100000) {
-			return super.getPreco() - super.getPreco() * 0.08;
+			return precoBase - precoBase * 0.08;
 		} else {
-			return getPreco(); 
+			return precoBase; 
 		}
 	}
-	
-	
-	
 
+	public void reduzirPreco() {
+		setPreco(getPreco() - getPreco() * 0.08);
+	}
+
+	public double getKm() {
+		return km;
+	}
 }
